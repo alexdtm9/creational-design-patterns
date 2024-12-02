88131ff823bb6453898fd5d1c1fa127dc307d2ff
alexdtm9 <60963447+alexdtm9@users.noreply.github.com>
2024-12-02 18:42:56 +0100

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Before first access");
+Singleton singleton1 = Singleton.Instance;
+Console.WriteLine("After first access");
+Singleton singleton2 = Singleton.Instance;
+
+// // This is also thread-safe. Comment above code and uncomment below code to see it in action.
+// ParallelEnumerable.Range(0, 1000)
+//     .ForAll(_ =>
+//     {
+//         Singleton singleton = Singleton.Instance;
+//     });
+
+sealed class Singleton
+{
+    private static readonly Lazy<Singleton> _lazyInstance = new(() => new());
+
+    public static Singleton Instance => _lazyInstance.Value;
+
+    private Singleton()
+    {
+        Console.WriteLine("Instantiating singleton");
+    }
+}
---
+++ Singleton/README.md
@@ -0,0 +1,6 @@
+# Singleton Pattern
+
+### Characteristics
+- Ensure an object only has one instance.
+- Provides easy access to the one instance.
+- Allows objects to control their instantiation process.
